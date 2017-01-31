5001089ec88c8f5b6f1ffc938868d45b7fd9c49b
M.Vainauskas <mindelis1@gmail.com>
2017-01-31 16:56:57 +0000

    Initial Commit
    First commit of new project repository. It contains basic springboot
    application with a rudimentary index.html page.

---
+++ clubManager2017/src/main/java/ie/manager/ClubManager2017.java
@@ -0,0 +1,12 @@
+package ie.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubManager2017 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubManager2017.class, args);
+	}
+}
