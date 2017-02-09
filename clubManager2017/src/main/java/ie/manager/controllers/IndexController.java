13f5fc1b3ed57f9d5b3d843156450c53e336a4a3
M.Vainauskas <mindelis1@gmail.com>
2017-02-09 18:59:48 +0000

    Added dependency for bootstrap css styles. Started working on designing front end of application. Added member and event pages to the template folder to be filled later.

--- clubManager2017/src/main/java/ie/manager/controllers/IndexController.java
+++ clubManager2017/src/main/java/ie/manager/controllers/IndexController.java
@@ -1,14 +1,13 @@
 package ie.manager.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import ie.manager.services.EventService;
 import ie.manager.services.MemberService;
 
 
-@RestController
+@Controller
 public class IndexController {
 	
 	@Autowired
