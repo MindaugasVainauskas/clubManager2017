2077d3d1838897de8d86b6aab16eef18e0db1200
M.Vainauskas <mindelis1@gmail.com>
2017-02-13 19:05:13 +0000

    Using Thymeleaf engine, data from database now is displayed in table format. Tables are styled with Bootstrap just like page design itself.

--- clubManager2017/src/main/java/ie/manager/controllers/IndexController.java
+++ clubManager2017/src/main/java/ie/manager/controllers/IndexController.java
@@ -2,7 +2,10 @@ package ie.manager.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import ie.manager.services.EventService;
 import ie.manager.services.MemberService;
 
@@ -18,14 +21,21 @@ public class IndexController {
 	@GetMapping("/")
 	String index(){
 		return "index";
+	}	
+	
+	@RequestMapping("/events")
+	String events(Model model){
+		model.addAttribute("eventList", eventService.listAllEvents());
+		return "events";
 	}
 	
-	@GetMapping("/member-list")
-	String memberList(){
-		return memberService.listAllMembers().toString();
+	@RequestMapping("/members")
+	String listMembers(Model model){
+		model.addAttribute("memberList", memberService.listAllMembers());
+		return "members";
 	}
 	
-	@GetMapping("/event-list")
+	@GetMapping("/listEvents")
 	String eventList(){
 		return eventService.listAllEvents().toString();
 	}
